ff7d1181841af33a7cebb1fff95d643965846b8d
sweta <swetakothari541@gmail.com>
2023-01-18 00:56:27 +0530

    solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,29 @@
 package com.driver;
 
+import jdk.dynalink.beans.StaticClass;
+
 public class Main {
-  
+
+    static class A{
+
+        public String meth(){
+            return "invoking method from class A";
+        }
+    }
+
+    static class B extends A{
+
+        @Override
+        public String meth(){
+            return "Method in extendend class B";
+        }
+    }
+
+    public static void main(String[] args) {
+        B b = new B();
+        b.meth();
+
+        B b1 = new B();
+        b1.meth();
+    }
 }
